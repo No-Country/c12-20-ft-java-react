12333a92ef1a74f4a997e56742cb5349d7d3ccd6
Marcos Lopez <marcoslopezdev18@gmail.com>
2023-07-03 19:16:29 -0300

    buenas

--- backend/gym/src/main/java/c1220ftjavareact/gym/GymApplication.java
+++ backend/gym/src/main/java/c1220ftjavareact/gym/GymApplication.java
@@ -1,13 +1,18 @@
 package c1220ftjavareact.gym;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class GymApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GymApplication.class, args);
+		SpringApplication
+				.run(GymApplication.class, args);
+
+
 	}
 
 }
