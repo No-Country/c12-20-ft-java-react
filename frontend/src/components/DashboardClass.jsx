57ebd0e04b123d60335f871ec786eeaef29f8404
JailerWebdeveloper <jailerwebwork@gmail.com>
2023-07-13 00:48:20 -0500

    Falta el add de clases

--- frontend/src/components/DashboardClass.jsx
+++ frontend/src/components/DashboardClass.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { Fragment } from "react";
 import { useState } from "react";
-import usuariodefault from "../assets/usuariodefault.jpg";
 import {
-  RiBarChart2Line,
   RiSearch2Line,
-  RiDeleteBin5Fill,
-  RiSettings4Fill,
 } from "react-icons/ri";
 import Sidebar from "./shared/Sidebar";
+import CardClass from "./shared/CardClass";
+import ModalAddClass from "./shared/ModaladdClass";
 
 
 const DashboardClass = () => {
@@ -16,13 +14,88 @@ const DashboardClass = () => {
 
     const [Events, setEvents] = useState([
         {
+          Name: "Cycling",
           activityId: 2,
           roomId: 2,
+          Photo: "https://images.theconversation.com/files/317906/original/file-20200301-166496-1qxkfiq.jpg?ixlib=rb-1.1.0&rect=0%2C0%2C3594%2C2382&q=20&auto=format&w=320&fit=clip&dpr=2&usm=12&cs=strip",
           capacity: 10,
           timeStart: "09:30",
           timeEnd: "17:50",
           Day: "Monday",
         },    
+        {
+          Name: "Cycling",
+          activityId: 2,
+          roomId: 2,
+          Photo: "https://images.theconversation.com/files/317906/original/file-20200301-166496-1qxkfiq.jpg?ixlib=rb-1.1.0&rect=0%2C0%2C3594%2C2382&q=20&auto=format&w=320&fit=clip&dpr=2&usm=12&cs=strip",
+          capacity: 10,
+          timeStart: "09:30",
+          timeEnd: "17:50",
+          Day: "Monday",
+        },    
+        {
+          Name: "Cycling",
+          activityId: 2,
+          roomId: 2,
+          Photo: "https://images.theconversation.com/files/317906/original/file-20200301-166496-1qxkfiq.jpg?ixlib=rb-1.1.0&rect=0%2C0%2C3594%2C2382&q=20&auto=format&w=320&fit=clip&dpr=2&usm=12&cs=strip",
+          capacity: 10,
+          timeStart: "09:30",
+          timeEnd: "17:50",
+          Day: "Monday",
+        },    
+        {
+          Name: "Cacling",
+          activityId: 2,
+          roomId: 2,
+          Photo: "https://images.theconversation.com/files/317906/original/file-20200301-166496-1qxkfiq.jpg?ixlib=rb-1.1.0&rect=0%2C0%2C3594%2C2382&q=20&auto=format&w=320&fit=clip&dpr=2&usm=12&cs=strip",
+          capacity: 10,
+          timeStart: "09:30",
+          timeEnd: "17:50",
+          Day: "Monday",
+        },  
+        {
+          Name: "Cacling",
+          activityId: 2,
+          roomId: 2,
+          Photo: "https://images.theconversation.com/files/317906/original/file-20200301-166496-1qxkfiq.jpg?ixlib=rb-1.1.0&rect=0%2C0%2C3594%2C2382&q=20&auto=format&w=320&fit=clip&dpr=2&usm=12&cs=strip",
+          capacity: 10,
+          timeStart: "09:30",
+          timeEnd: "17:50",
+          Day: "Monday",
+        },  
+        
+        {
+          Name: "Cacling",
+          activityId: 2,
+          roomId: 2,
+          Photo: "https://images.theconversation.com/files/317906/original/file-20200301-166496-1qxkfiq.jpg?ixlib=rb-1.1.0&rect=0%2C0%2C3594%2C2382&q=20&auto=format&w=320&fit=clip&dpr=2&usm=12&cs=strip",
+          capacity: 10,
+          timeStart: "09:30",
+          timeEnd: "17:50",
+          Day: "Monday",
+        },  
+
+
+        {
+          Name: "Cacling",
+          activityId: 2,
+          roomId: 2,
+          Photo: "https://images.theconversation.com/files/317906/original/file-20200301-166496-1qxkfiq.jpg?ixlib=rb-1.1.0&rect=0%2C0%2C3594%2C2382&q=20&auto=format&w=320&fit=clip&dpr=2&usm=12&cs=strip",
+          capacity: 10,
+          timeStart: "09:30",
+          timeEnd: "17:50",
+          Day: "Monday",
+        },  
+        {
+          Name: "Cacling",
+          activityId: 2,
+          roomId: 2,
+          Photo: "https://images.theconversation.com/files/317906/original/file-20200301-166496-1qxkfiq.jpg?ixlib=rb-1.1.0&rect=0%2C0%2C3594%2C2382&q=20&auto=format&w=320&fit=clip&dpr=2&usm=12&cs=strip",
+          capacity: 10,
+          timeStart: "09:30",
+          timeEnd: "17:50",
+          Day: "Monday",
+        },  
       ]);
       const [search, setSearch] = useState("");
 
@@ -30,9 +103,16 @@ const DashboardClass = () => {
         setSearch(event.target.value);
       };
 
+      const Deleteuser = (id) => {
+        const updatedAdmins = Admins.filter((Admin) => Admin.id !== id);
+        setAdmins(updatedAdmins);
+        setAlertdel(true);
+      };
+
+
       const Filtered = search
       ? Events.filter((Clasess) =>
-          Clasess.name.toLowerCase().startsWith(search.toLowerCase())
+          Clasess.Name.toLowerCase().startsWith(search.toLowerCase())
         )
       : Events;
       
@@ -46,8 +126,9 @@ const DashboardClass = () => {
             {/*Main*/}
             <div className="p-10 w-full flex flex-col h-full items-center">
               <div className="flex w-full justify-between items-center">
-                <h1 className="text-blue-600 text-2xl">Clasess</h1>
+                <h1 className="text-2xl font-bold text-blue-600">Clasess</h1>
                 <div className="flex items-center gap-2">
+                  <ModalAddClass addClass={setEvents} />
                 <input
                   type="text"
                   placeholder="Search"
@@ -59,7 +140,18 @@ const DashboardClass = () => {
               </div>
                 </div>
               {/*Table*/}
-              <div className="flex flex-col w-full mt-10">  
+              <div className="flex flex-col   w-full mt-5">  
+              <div className="grid grid-cols-1 overflow-y-auto max-h-[501px] sm:grid-cols-2 md:grid-cols-3 gap-4 p-10 place-items-center my-5">
+
+        
+              {Filtered.map ((Clasess) => (
+                 <React.Fragment key={Clasess.activityId}>
+                  <div className="my-2  w-full flex justify-center">
+                  <CardClass Clasess={Clasess} />
+                  </div>
+                 </React.Fragment>
+                 ))} 
+                 </div>
             </div>
           </div>
           </div>
--- frontend/src/components/shared/CardClass.jsx
+++ frontend/src/components/shared/CardClass.jsx
@@ -1,22 +1,35 @@
-const CardClass = () => {
+import React, { useState } from 'react';
+
+const CardClass = ({ Clasess }) => {
+  const { activityId, roomId, capacity, timeStart, timeEnd, Day, Photo, Name } = Clasess;
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+  };
+
   return (
     <>
-      <div className="card card-compact w-4-5 bg-base-100 shadow-xl">
-        <figure>
-          <img
-            src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-            alt="Shoes"
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
+    <div className="card w-5/6 bg-base-100 shadow-2xl  ease-in duration-300 delay-150 hover:ring ">
+      <figure className="max-h-56">
+    
+        <img
+          src={Photo}
+          alt="image"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">
+          {Name}
+        </h2>
+        <p>{Day}</p>
+        <p>{capacity}</p>
+        <div className="card-actions justify-end">
+          <div className="badge badge-outline truncate">{timeEnd}-{timeStart}</div>
         </div>
       </div>
-    </>
+    </div>
+  </>
   );
 };
 
